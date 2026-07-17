f5cd5719dee68bf81b286f65e026a033ec9fd1c6
alichac-afk <alicha.c@kkumail.com>
2026-07-17 14:17:36 +0700

    feat: update student info - ID 6733804317 sec 3

--- src/main/java/com/example/lab04/controller/HelloController.java
+++ src/main/java/com/example/lab04/controller/HelloController.java
@@ -1,4 +1,4 @@
-package com.example.lab03.controller;
+package com.example.lab04.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -28,9 +28,9 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloController {
 
     // ── ข้อมูลนักศึกษา ── แก้ไขตรงนี้ ──────────────────────────────────────
-    private static final String NAME    = "Your Name Here";     // TODO: ใส่ชื่อจริง
-    private static final String ID      = "xxxx";               // TODO: ใส่รหัสนักศึกษา
-    private static final String SECTION = "x";                  // TODO: ใส่เลข Section
+    private static final String NAME    = "อลิชา ชนะบุญ";     // TODO: ใส่ชื่อจริง
+    private static final String ID      = "673380431-7";               // TODO: ใส่รหัสนักศึกษา
+    private static final String SECTION = "3";                  // TODO: ใส่เลข Section
     // ─────────────────────────────────────────────────────────────────────────
 
     /**
